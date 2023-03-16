b11dc91be045b2c2cd85a7b2e1375b417698fe2b
David Pineda <david.pineda.orvc@gmail.com>
2023-03-16 19:34:52 -0400

    My understanding

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,7 +1,9 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
 
+  // find equal increment
   const jmpAmount = Math.floor(Math.sqrt(breaks.length));
 
+  // First walk
   let i = jmpAmount;
   for (; i < breaks.length; i += jmpAmount) {
     if (breaks[i]) {
@@ -9,13 +11,16 @@ export default function two_crystal_balls(breaks: boolean[]): number {
     }
   }
 
+  // back to last unbroken point
   i -= jmpAmount;
 
+  // walk back to true breaking point
   for (let j = 0; j < jmpAmount && i < breaks.length; ++j, ++i) {
     if (breaks[i]) {
       return i;
     }
   }
 
+  // sentinel
   return -1;
 }
